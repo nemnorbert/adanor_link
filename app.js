cc5d0ea66b451a8385e47b10e50f14b36ac1316d
Norbert Nemeth <nth.norbert@gmail.com>
2024-02-21 13:06:26 +0100

    minor fix

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
 const axios = require('axios');
 
 const errorHandler = require('./utilities/errorHandler.js');
 
 app.get('/', (req, res, next) => {
     const link = `https://adanor.eu/link`;
-    console.log(`Redirect To: ${link}`);
-    //res.redirect(302, link);
-    res.json({
-        success: false,
-        status: '400',
-        message: 'Home Page',
-    });
+    res.redirect(302, link);
 })
 
 app.get('/:id', async (req, res, next) => {
     try {
-        // Validation
-        const schema = Joi.object({
-            id: Joi.string().pattern(/^[\w\-]{12,15}$/).required(),
-        });
-        await schema.validateAsync(req.params);
-    } catch (error) {
-        return errorHandler(400, error, res);
-    }
-
-    try {
         const id = req.params.id;
+        const subid = req.query.id || null;
+
         const isProduction = process.env.NODE_ENV === 'production';
         const baseURL = isProduction ? 'https://api.adanor.eu' : 'http://localhost:8080';
-        const response = await axios.get(`${baseURL}/links/${id}`);
+        let fullUrl = `${baseURL}/links/${id}`;
+        if (subid) {fullUrl += `?id=${subid}`;}
+
+        const response = await axios.get(fullUrl);
         const data = response.data;
 
-        if (data.blocked) {
-            return errorHandler(410, 'Link is Blocked', res);
-        }
-        if (data.success) {
-            console.log(`Redirect To: ${data.link}`);
-            res.redirect(302, data.link);
-        }
+        res.redirect(302, data.redirect_url);
     } catch (error) {
-        const status = error.response.status || 500;
-        const message = error.response.data.message || 'API Connection Error';
-        return errorHandler(status, message, res);
+        if (error.code === 'ECONNREFUSED') {
+            return errorHandler(503, 'API Service Unavailable', res);
+        } else if (error.response) {
+            return errorHandler(error.response.status, error.response.data.message, res);
+        } else {
+            console.error(error);
+            return errorHandler(500, 'Internal Server Error', res);
+        }
     }
 })
+
 
 module.exports = app;
