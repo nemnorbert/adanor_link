550c03ca1be5ef1d8eb21500ddf241c7941b4da7
Norbert Nemeth <nth.norbert@gmail.com>
2024-02-25 00:43:36 +0100

    lang param

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
 const errorHandler = require('./utilities/errorHandler.js');
 
 app.get('/', (req, res, next) => {
@@ -15,9 +14,17 @@ app.get('/:id', async (req, res, next) => {
         const subid = req.query.id || null;
 
         const isProduction = process.env.NODE_ENV === 'production';
-        const baseURL = isProduction ? 'https://api.adanor.eu' : 'http://localhost:8080';
-        let fullUrl = `${baseURL}/links/${id}`;
-        if (subid) {fullUrl += `?id=${subid}`;}
+        let fullUrl = isProduction ? 'https://api.adanor.eu' : 'http://localhost:8080';
+        fullUrl += `/links/${id}`;
+
+        const params = new URLSearchParams();
+        if (subid) {params.append('id', subid);}
+
+        const browserLang = req.headers['accept-language']?.substring(0, 2);
+        if (browserLang) {params.append('lang', browserLang);}
+
+        const queryString = params.toString() ?? null;
+        if (queryString) {fullUrl += `?${queryString}`;}
 
         const response = await axios.get(fullUrl);
         const data = response.data;
@@ -25,12 +32,12 @@ app.get('/:id', async (req, res, next) => {
         res.redirect(302, data.redirect_url);
     } catch (error) {
         if (error.code === 'ECONNREFUSED') {
-            return errorHandler(503, 'API Service Unavailable', res);
+            return errorHandler(503, 'API Service Unavailable', req, res);
         } else if (error.response) {
-            return errorHandler(error.response.status, error.response.data.message, res);
+            return errorHandler(error.response.status, error.response.data.message, req, res);
         } else {
             console.error(error);
-            return errorHandler(500, 'Internal Server Error', res);
+            return errorHandler(500, 'Internal Server Error', req, res);
         }
     }
 })
--- utilities/errorHandler.js
+++ utilities/errorHandler.js
@@ -1,13 +1,24 @@
-const errorHandler = (status, message, res) => {
+const errorHandler = (status, message, req, res) => {
     try {
         const error = new Error(message);
         error.status = status || 500;
 
-        res.status(error.status).json({
-            success: false,
-            status: error.status,
-            message: error.message,
-        });
+        const isProduction = process.env.NODE_ENV === 'production';
+        if (!isProduction) {
+            return res.status(error.status).json({
+                success: false,
+                status: error.status,
+                message: error.message,
+            });
+        }
+
+        const siteUrl = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+        let link = `https://adanor.eu/support?err=${error.status}`;
+        link += `&text=${encodeURIComponent(error.message)}` || '';
+        link += `&site=${encodeURIComponent(siteUrl)}` || '';
+
+        res.redirect(302, link);
+
     } catch (error) {
         console.log(error);
     }
